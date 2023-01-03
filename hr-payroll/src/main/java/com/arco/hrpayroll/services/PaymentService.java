46259c5406f317450285349b59c15b2d2fd36f0b
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-01-03 14:17:34 -0500

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/arco/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.arco.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.arco.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
