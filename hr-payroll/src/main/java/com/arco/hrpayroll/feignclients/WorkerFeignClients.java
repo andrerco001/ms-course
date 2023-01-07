920b51300a69297545c524e9925f647e9e8e9466
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-01-07 16:33:43 -0500

    Ribbon load balancing

--- hr-payroll/src/main/java/com/arco/hrpayroll/feignclients/WorkerFeignClients.java
+++ hr-payroll/src/main/java/com/arco/hrpayroll/feignclients/WorkerFeignClients.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.arco.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(value = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(value = "hr-worker", path = "/workers")
 public interface WorkerFeignClients {
 	@GetMapping(value = "/{id}")
 	ResponseEntity<Worker> findById(@PathVariable Long id);
